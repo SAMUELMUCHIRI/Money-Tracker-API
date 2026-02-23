dd2336347307822a3c7070bf39fe886f495bee2b
SAMUELMUCHIRI <eng.njaumuchiri@gmail.com>
2026-02-24 01:53:37 +0300

    Middleware: To handle Authentication of Requests

---
+++ app/Http/Middleware/api_auth.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class api_auth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!auth("sanctum")->check()) {
+            return response()->json(
+                [
+                    "message" => "Unauthenticated",
+                    "error" => "Invalid or missing authentication token",
+                ],
+                401,
+            );
+        }
+
+        return $next($request);
+    }
+}
